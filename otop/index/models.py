67955889296c440b4d725456c4befc13c8301df9
Mr. Ekarat <ekarat.income@gmail.com>
2024-05-17 16:18:38 +0700

    Add files via upload
    Uncle Engineer ep5

--- otop/index/models.py
+++ otop/index/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Tracking(models.Model):
+     name = models.CharField(max_length=100)
+     tel = models.CharField(max_length=100)
+     Tracking = models.CharField(max_length=100, null=True, blank=True)
+     #other = models.TextChoices(null=False, blank=True)
+
+
+     def __str__(self):
+        return '{} - {} ({})'.format(self.name, self.tel, self.Tracking)
+
+         
